d0d59a7d6b0dbd18a7acddf0c9d55e97883c706e
Terry <139153979+logicalrabbit@users.noreply.github.com>
2023-09-02 23:49:34 +0300

    Modify side bar trigger styles

--- src/components/mobile-side-bar.jsx
+++ src/components/mobile-side-bar.jsx
@@ -9,7 +9,7 @@ import { Sidebar } from './sidebar';
 function MobileSidebar() {
   return (
     <Sheet>
-      <SheetTrigger className="md:hidden mb-3">
+      <SheetTrigger className="w-min md:hidden mb-3 p-2">
         <List size={26} className='text-inherit' />
       </SheetTrigger>
       <SheetContent className='w-80 p-0' side='left'>
